c0f7e38899d8e2b8460b18576cdc24580ff92784
r.parlett <r.parlett@localhost>
2008-07-01 16:01:53 +0000

    New

---
+++ config/files/base/h/define.h
@@ -0,0 +1,13 @@
+/*  
+ * Icon configuration file for Unix, built using GNU autoconf.
+ */
+
+#define UNIX 1
+
+#ifdef HAVE_LIBDL
+#define LoadFunc 1
+#endif
+
+#ifdef HAVE_LIBX11
+#define Graphics 1
+#endif
