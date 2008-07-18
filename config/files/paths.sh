8c16db2948693aab8a139af0647961d9e67c606b
r.parlett <r.parlett@localhost>
2008-07-18 18:05:21 +0000

    Some improvements to paths.sh

---
+++ config/files/paths.sh
@@ -0,0 +1,8 @@
+# Object Icon path settings
+export OIHOME="@CONFIG_DIR@"
+export OIBIN="$OIHOME/bin"
+export OILIB="$OIHOME/lib"
+export OIPATH="$OILIB/main:$OILIB/gui:$OILIB/xml:$OILIB/parser:$OILIB/ipl"
+export OLPATH="$OILIB/incl"
+PATH="$PATH:$OIBIN"
+export TRACE
