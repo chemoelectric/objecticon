119d1bcdde2cd1ad47619b18979f54fa4f522f35
r.parlett <r.parlett@localhost>
2008-09-09 02:20:14 +0000

    Extra keyword tests

--- test/mt.sh
+++ test/mt.sh
@@ -7,7 +7,8 @@ oit -s mt_c1.icn utils.icn
 oit -s mt_c2.icn utils.icn
 oit -s mt_c3.icn utils.icn
 oit -s mt_p1.icn utils.icn
+oit -s kwds.icn utils.icn
 ./mt_p1 >mt.out
-rm -f mt_c1 mt_c2  mt_c3  mt_p1
+rm -f mt_c1 mt_c2  mt_c3  mt_p1 kwds
 set -e
 cmp mt.out mt.std
