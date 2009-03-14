9a9a2f70de57d62bf2a265fef40ea68a42ee82ac
r.parlett <r.parlett@localhost>
2009-03-14 14:44:19 +0000

    New

---
+++ test/classes.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+rm -f packages.txt
+oit -s -c classes_packtest?.icn
+oit -l 2 -s classes.icn utils.icn
+./classes >classes.out
+rm -f classes classes_packtest?.u
+set -e
+cmp classes.out classes.std
