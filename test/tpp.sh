09f45a66e93b4ef7c559595980721197503633e7
r.parlett <r.parlett@localhost>
2009-09-09 15:39:09 +0000

    tpp test

--- test/tpp.sh
+++ test/tpp.sh
@@ -2,8 +2,10 @@
 #
 #  special test of preprocessing and related error detection
 
+touch empty
 oit -E tpp.icn >tpp.out 2>tpp.err
 set -e
 cmp tpp.out tpp.std
 cmp tpp.err tpp.stde
-rm -f tpp.err
+rm -f tpp.err empty
+
