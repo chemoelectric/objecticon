530ef583ac81cc2dc63d4a0cd86cb328ce590601
r.parlett <r.parlett@localhost>
2010-05-09 19:24:42 +0000

    mouse/cons loops now have timer fd allocated outside

--- base/h/graphics.h
+++ base/h/graphics.h
@@ -344,7 +344,8 @@ typedef struct _wstate {
   Image         *pix;
   char          mount_dir[64];
   int           event_pipe[2];
-  int           wsys_fd, wctl_fd, mouse_fd, cons_fd, consctl_fd, cursor_fd;
+  int           wsys_fd, wctl_fd, mouse_fd, cons_fd, consctl_fd, cursor_fd,
+                   mouse_time_fd, cons_time_fd;
   int           mouse_pid, cons_pid;
   int           mouse_down;
   int           last_mouse_x, last_mouse_y;
