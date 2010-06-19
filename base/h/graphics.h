a7030eb9945783c31af77255fe0d843319a2acf1
r.parlett <r.parlett@localhost>
2010-06-19 13:57:45 +0000

    Got rid of wmhintflags field

--- base/h/graphics.h
+++ base/h/graphics.h
@@ -284,7 +284,6 @@ typedef struct _wstate {
   int		normalw, normalh;	/* size to remember when maximized */
   int		state;			/* window state; icon, window or root*/
   Window        transientfor;           /* transient-for hint */
-  long		wmhintflags;		/* window manager hints */
 #elif MSWIN32
   HWND		win;			/* client window */
   HWND		iconwin;		/* client window when iconic */
