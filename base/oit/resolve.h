0a450698d9ca4e0201f30c640af1b8e99f997c65
r.parlett <r.parlett@localhost>
2011-07-23 15:34:15 +0000

    Needed void in func declaration

--- base/oit/resolve.h
+++ base/oit/resolve.h
@@ -5,6 +5,6 @@ void resolve_local(struct lfunction *func, struct lentry *lp);
 void resolve_supers(void);
 void compute_inheritance(void);
 void resolve_invocables(void);
-void add_functions();
+void add_functions(void);
 
 #endif
