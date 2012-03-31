3775fa775cb84ca54a5897c357999a3efe436562
r.parlett <r.parlett@localhost>
2012-03-31 13:05:13 +0000

    Now use OINATIVE path to lookup .so libs

--- config/files/paths.sh
+++ config/files/paths.sh
@@ -4,6 +4,6 @@ export OIBIN="$OIHOME/bin"
 export OILIB="$OIHOME/lib"
 export OIPATH="$OILIB/main:$OILIB/gui:$OILIB/xml:$OILIB/parser:$OILIB/ipl"
 export OIINCL="$OILIB/incl"
+export OINATIVE="$OILIB/native"
 PATH="$PATH:$OIBIN"
-LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$OILIB/native"
 export TRACE OIMAXLEVEL
