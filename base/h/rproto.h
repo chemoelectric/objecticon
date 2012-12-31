18a97cf052730382cb0e29fca574514190a9e0d5
r.parlett <r.parlett@localhost>
2012-12-31 19:55:54 +0000

    Reverted get_pattern

--- base/h/rproto.h
+++ base/h/rproto.h
@@ -279,7 +279,6 @@ void drawimgdata(wbp w, word x, word y, word width, word height, struct imgdata 
    int  sendselectionresponse(wbp w, word requestor, char *property, char *selection, char *target, word time, dptr data);
    int  setwindowicon   (wbp w, struct imgdata *imd);
    int  setpattern  (wbp w, struct imgdata *imd);
-   int  getpattern  (wbp w, struct imgdata *imd);
    int  setwindowlabel  (wbp w, char *val);
    int  walert          (wbp w, int volume);
    int  warppointer     (wbp w, int x, int y);
