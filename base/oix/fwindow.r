9109cf42abe554e4e9089e951965d72863e4ed91
r.parlett <r.parlett@localhost>
2013-01-31 12:56:32 +0000

    get_pattern put back

--- base/oix/fwindow.r
+++ base/oix/fwindow.r
@@ -567,6 +567,20 @@ function graphics_Window_lower(self)
    }
 end
 
+function graphics_Window_get_pattern_impl(self)
+   body {
+      struct imgdata *imd;
+      GetSelfW();
+      MemProtect(imd = malloc(sizeof(struct imgdata)));
+      imd->paltbl = 0;
+      if (getpattern(self_w, imd) == Failed) {
+          free(imd);
+          fail;
+      }
+      return C_integer((word)imd);
+   }
+end
+
 function graphics_Window_get_pixels_impl(self, x0, y0, w0, h0)
    body {
       struct imgdata *imd;
