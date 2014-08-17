b29d5ef8cdbbde9c73ed64020bbad11a27b1eeea
r.parlett <r.parlett@localhost>
2014-08-17 22:22:14 +0000

    Merges

--- base/h/grttin.h
+++ base/h/grttin.h
@@ -343,7 +343,7 @@ if (!(w))
     runerr(142, p);
 #enddef
 
-#begdef AttemptAttr(operation, reason)
+#begdef GenAttemptAttr(operation, reason, cleanup)
 do {
    tended struct descrip saved_why;
    saved_why = kywd_why;
@@ -364,12 +364,37 @@ do {
            fail;
        }
        default: {
-           syserr("Invalid return code from graphics op"); 
-           fail;
+           syserr("Invalid return code from op"); 
+           break;
        }
    }
 } while(0)
 #enddef
+
+#begdef GenAttemptOp(operation, cleanup)
+do {
+   switch (operation) { 
+       case Error: {
+           runerr(0); 
+           break;
+       }
+       case Succeeded: {
+           break;
+       }
+       case Failed: {
+           cleanup;
+           fail;
+       }
+       default: {
+           syserr("Invalid return code from op"); 
+           break;
+       }
+   }
+} while(0)
+#enddef
+
+#define AttemptOp(operation) GenAttemptOp(operation,)
+#define AttemptAttr(operation, reason) GenAttemptAttr(operation, reason,)
 
 #begdef FdStaticParam(p, m)
 int m;
