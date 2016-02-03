5b5ebdc7f3a36db07f265da241e374426532c22c
rparlett <rparlett@localhost>
2016-02-03 23:14:57 +0000

    Major cairo improvements

--- lib/native/gcairo.h
+++ lib/native/gcairo.h
@@ -1,4 +1,5 @@
 typedef int cairo_surface_t, cairo_t, cairo_format_t, cairo_matrix_t, cairo_pattern_t,
     cairo_path_t, cairo_path_data_t, cairo_operator_t, PangoLayout, PangoFontDescription,
     uint32_t, cairo_user_data_key_t, cairo_rectangle_t, RsvgHandle, RsvgDimensionData,
-    PangoContext, RsvgPositionData, GError, guint8, cairo_rectangle_list_t, cairo_antialias_t;
+    PangoContext, RsvgPositionData, GError, guint8, cairo_rectangle_list_t, cairo_antialias_t,
+    cairo_filter_t, cairo_status_t;
