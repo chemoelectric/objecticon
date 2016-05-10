598a2588bb60ab6e186f782d26f0c0e5f13aa42d
rparlett <rparlett@localhost>
2016-05-10 11:21:15 +0000

    Merges

--- base/oix/native.r
+++ base/oix/native.r
@@ -3937,6 +3937,12 @@ function lang_Internal_hash(x)
    }
 end
 
+function lang_Internal_order(x)
+   body {
+      return C_integer order(&x);
+   }
+end
+
 function lang_Coexpression_traceback(ce, act_chain)
    body {
        tended struct b_coexpr *b;
