74cd8957ad4d2fba906993dc5320a1a1d8e3a935
rparlett <rparlett@localhost>
2016-05-30 12:24:19 +0000

    Include error code in name lookup failure string

--- base/oix/native.r
+++ base/oix/native.r
@@ -1735,7 +1735,7 @@ static void getaddrinfo_error2why(int error)
     if (error == EAI_SYSTEM)
         errno2why();
     else
-        whyf("Name lookup failure: %s", gai_strerror(error));
+        whyf("Name lookup failure: %s (gai errno=%d)", gai_strerror(error), error);
 }
 
 static struct sockaddr *parse_sockaddr(char *s, int *len)
