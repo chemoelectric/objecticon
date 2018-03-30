d7ef7ea88a7e7b1c23b384c38c11bea5d44f471c
rparlett <rparlett@localhost>
2018-03-30 20:31:24 +0000

    Merges

--- base/oit/tlex.c
+++ base/oit/tlex.c
@@ -655,7 +655,7 @@ static struct toktab *getucs(int ac, int *cc)
         char *t = p;
         int i = utf8_check(&p, lex_sbuf.endimage);
         if (i == -1) {
-            lexfatal("Invalid utf-8 sequence beginning at char %d", 1 + (t - lex_sbuf.strtimage));
+            lexfatal("Invalid UTF-8 sequence beginning at char %d", 1 + (t - lex_sbuf.strtimage));
             break;
         }
         if (i < 0 || i > MAX_CODE_POINT) {
@@ -976,7 +976,7 @@ static int read_utf_char(int c)
     char utf8[MAX_UTF8_SEQ_LEN], *p = utf8;
     int i, n = UTF8_SEQ_LEN(c);
     if (n < 1) {
-        lexfatal("Invalid utf-8 start char");
+        lexfatal("Invalid UTF-8 start char");
         return ' ';  /* Returning space keeps down follow-through error messages */
     }
     utf8[0] = c;
@@ -988,7 +988,7 @@ static int read_utf_char(int c)
     }
     c = utf8_check(&p, utf8 + n);
     if (c == -1) {
-        lexfatal("Invalid utf-8 sequence");
+        lexfatal("Invalid UTF-8 sequence");
         return ' ';
     }
     if (c < 0 || c > MAX_CODE_POINT) {
