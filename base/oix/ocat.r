26d25868c5ef9684d6710af52a6a1f50d2da4e70
rparlett <rparlett@localhost>
2018-05-31 17:37:08 +0000

    Ascii/ucs combination in string catenation

--- base/oix/ocat.r
+++ base/oix/ocat.r
@@ -8,55 +8,153 @@ operator || cat(x, y)
      if (need_ucs(&x) || need_ucs(&y)) {
          tended struct descrip utf8_x, utf8_y, utf8;
 
-         if (!cnv:ucs(x, x))
-             runerr(128, x);
-         if (!cnv:ucs(y, y))
-             runerr(128, y);
+         if (is_ascii_string(&x)) {
+             if (!cnv:ucs(y, y))
+                 runerr(128, y);
 
-         utf8_x = UcsBlk(x).utf8;
-         utf8_y = UcsBlk(y).utf8;
+             utf8_y = UcsBlk(y).utf8;
 
-         /*
-          *  Optimization 0:  Check for zero-length operands.
-          */
-
-         if (StrLen(utf8_x) == 0)
-             return y;
-         if (StrLen(utf8_y) == 0)
-             return x;
-
-         /*
-          *  Optimization 1:  The strings to be concatenated are already
-          *   adjacent in memory; no allocation is required.
-          */
-         if (StrLoc(utf8_x) + StrLen(utf8_x) == StrLoc(utf8_y)) {
-             MakeStr(StrLoc(utf8_x), StrLen(utf8_x) + StrLen(utf8_y), &utf8);
-         }
-         /*
-          * Optimization 2: The end of x is at the end of the string space.
-          *  Hence, x was the last string allocated and need not be
-          *  re-allocated. y is appended to the string space and the
-          *  result is pointed to the start of x.
-          */
-         else if ((StrLoc(utf8_x) + StrLen(utf8_x) == strfree) &&
-                  (DiffPtrs(strend,strfree) > StrLen(utf8_y))) {
              /*
-              * Append y to the end of the string space.
+              *  Optimization 0:  Check for zero-length operands.
               */
-             MemProtect(alcstr(StrLoc(utf8_y),StrLen(utf8_y)));
-             MakeStr(StrLoc(utf8_x), StrLen(utf8_x) + StrLen(utf8_y), &utf8);
+
+             if (StrLen(x) == 0)
+                 return y;
+             if (StrLen(utf8_y) == 0)
+                 return ucs(make_ucs_block(&x, StrLen(x)));
+
+             /*
+              *  Optimization 1:  The strings to be concatenated are already
+              *   adjacent in memory; no allocation is required.
+              */
+             if (StrLoc(x) + StrLen(x) == StrLoc(utf8_y))
+                 MakeStr(StrLoc(x), StrLen(x) + StrLen(utf8_y), &utf8);
+
+             /*
+              * Optimization 2: The end of x is at the end of the string space.
+              *  Hence, x was the last string allocated and need not be
+              *  re-allocated. y is appended to the string space and the
+              *  result is pointed to the start of x.
+              */
+             else if ((StrLoc(x) + StrLen(x) == strfree) &&
+                      (DiffPtrs(strend,strfree) > StrLen(utf8_y))) {
+                 /*
+                  * Append y to the end of the string space.
+                  */
+                 MemProtect(alcstr(StrLoc(utf8_y), StrLen(utf8_y)));
+                 MakeStr(StrLoc(x), StrLen(x) + StrLen(utf8_y), &utf8);
+             }
+             /*
+              * Otherwise, allocate space for x and y, and copy them
+              *  to the end of the string space.
+              */
+             else {
+                 MakeStrMemProtect(alcstr(NULL, StrLen(x) + StrLen(utf8_y)), 
+                                   StrLen(x) + StrLen(utf8_y), &utf8);
+                 memcpy(StrLoc(utf8), StrLoc(x), StrLen(x));
+                 memcpy(StrLoc(utf8) + StrLen(x), StrLoc(utf8_y), StrLen(utf8_y));
+             }
+             return ucs(make_ucs_block(&utf8, StrLen(x) + UcsBlk(y).length));
+         } else if (is_ascii_string(&y)) {
+
+             if (!cnv:ucs(x, x))
+                 runerr(128, x);
+
+             utf8_x = UcsBlk(x).utf8;
+
+             /*
+              *  Optimization 0:  Check for zero-length operands.
+              */
+
+             if (StrLen(utf8_x) == 0)
+                 return ucs(make_ucs_block(&y, StrLen(y)));
+             if (StrLen(y) == 0)
+                 return x;
+
+             /*
+              *  Optimization 1:  The strings to be concatenated are already
+              *   adjacent in memory; no allocation is required.
+              */
+             if (StrLoc(utf8_x) + StrLen(utf8_x) == StrLoc(y))
+                 MakeStr(StrLoc(utf8_x), StrLen(utf8_x) + StrLen(y), &utf8);
+
+             /*
+              * Optimization 2: The end of x is at the end of the string space.
+              *  Hence, x was the last string allocated and need not be
+              *  re-allocated. y is appended to the string space and the
+              *  result is pointed to the start of x.
+              */
+             else if ((StrLoc(utf8_x) + StrLen(utf8_x) == strfree) &&
+                      (DiffPtrs(strend,strfree) > StrLen(y))) {
+                 /*
+                  * Append y to the end of the string space.
+                  */
+                 MemProtect(alcstr(StrLoc(y), StrLen(y)));
+                 MakeStr(StrLoc(utf8_x), StrLen(utf8_x) + StrLen(y), &utf8);
+             }
+             /*
+              * Otherwise, allocate space for x and y, and copy them
+              *  to the end of the string space.
+              */
+             else {
+                 MakeStrMemProtect(alcstr(NULL, StrLen(utf8_x) + StrLen(y)), 
+                                   StrLen(utf8_x) + StrLen(y), &utf8);
+                 memcpy(StrLoc(utf8), StrLoc(utf8_x), StrLen(utf8_x));
+                 memcpy(StrLoc(utf8) + StrLen(utf8_x), StrLoc(y), StrLen(y));
+             }
+             return ucs(make_ucs_block(&utf8, UcsBlk(x).length + StrLen(y)));
+
+         } else {
+             if (!cnv:ucs(x, x))
+                 runerr(128, x);
+             if (!cnv:ucs(y, y))
+                 runerr(128, y);
+
+             utf8_x = UcsBlk(x).utf8;
+             utf8_y = UcsBlk(y).utf8;
+
+             /*
+              *  Optimization 0:  Check for zero-length operands.
+              */
+
+             if (StrLen(utf8_x) == 0)
+                 return y;
+             if (StrLen(utf8_y) == 0)
+                 return x;
+
+             /*
+              *  Optimization 1:  The strings to be concatenated are already
+              *   adjacent in memory; no allocation is required.
+              */
+             if (StrLoc(utf8_x) + StrLen(utf8_x) == StrLoc(utf8_y))
+                 MakeStr(StrLoc(utf8_x), StrLen(utf8_x) + StrLen(utf8_y), &utf8);
+
+             /*
+              * Optimization 2: The end of x is at the end of the string space.
+              *  Hence, x was the last string allocated and need not be
+              *  re-allocated. y is appended to the string space and the
+              *  result is pointed to the start of x.
+              */
+             else if ((StrLoc(utf8_x) + StrLen(utf8_x) == strfree) &&
+                      (DiffPtrs(strend,strfree) > StrLen(utf8_y))) {
+                 /*
+                  * Append y to the end of the string space.
+                  */
+                 MemProtect(alcstr(StrLoc(utf8_y), StrLen(utf8_y)));
+                 MakeStr(StrLoc(utf8_x), StrLen(utf8_x) + StrLen(utf8_y), &utf8);
+             }
+             /*
+              * Otherwise, allocate space for x and y, and copy them
+              *  to the end of the string space.
+              */
+             else {
+                 MakeStrMemProtect(alcstr(NULL, StrLen(utf8_x) + StrLen(utf8_y)), 
+                                   StrLen(utf8_x) + StrLen(utf8_y), &utf8);
+                 memcpy(StrLoc(utf8), StrLoc(utf8_x), StrLen(utf8_x));
+                 memcpy(StrLoc(utf8) + StrLen(utf8_x), StrLoc(utf8_y), StrLen(utf8_y));
+             }
+             return ucs(make_ucs_block(&utf8, UcsBlk(x).length + UcsBlk(y).length));
          }
-         /*
-          * Otherwise, allocate space for x and y, and copy them
-          *  to the end of the string space.
-          */
-         else {
-             MakeStrMemProtect(alcstr(NULL, StrLen(utf8_x) + StrLen(utf8_y)), 
-                               StrLen(utf8_x) + StrLen(utf8_y), &utf8);
-             memcpy(StrLoc(utf8), StrLoc(utf8_x), StrLen(utf8_x));
-             memcpy(StrLoc(utf8) + StrLen(utf8_x), StrLoc(utf8_y), StrLen(utf8_y));
-         }
-         return ucs(make_ucs_block(&utf8, UcsBlk(x).length + UcsBlk(y).length));
      } else {
          tended struct descrip result;
 
