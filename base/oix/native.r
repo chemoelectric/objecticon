0f2b8b8852befd7bd2c929642d02e3cb6d1d013b
rparlett <rparlett@localhost>
2019-07-21 14:49:09 +0000

    Avoid resource leak in get_handle()

--- base/oix/native.r
+++ base/oix/native.r
@@ -1164,7 +1164,7 @@ static struct b_proc *try_load(void *handle, struct b_class *class0,  struct cla
 
     /* Sanity check. */
     if (blk->title != T_Proc)
-        ffatalerr("lang.Class.load_library(): symbol %s not a procedure block", fq);
+        ffatalerr("lang.Class.load_library(): Symbol '%s' not a procedure block", fq);
 
     free(fq);
 
@@ -1202,10 +1202,12 @@ static void *get_handle(char *filename)
     /* Check version number */
     version = (int *)dlsym(handle, "oix_version");
     if (!version) {
+        dlclose(handle);
         LitWhy("Symbol 'oix_version' not found");
         return 0;
     }
     if (*version != OixVersion) {
+        dlclose(handle);
         whyf("Version mismatch (%d -vs- %d)", *version, OixVersion);
         return 0;
     }
@@ -1296,7 +1298,7 @@ function lang_Proc_load(filename, funcname)
        }
        /* Sanity check. */
        if (blk->title != T_Proc)
-           ffatalerr("lang.Proc.load(): symbol %s not a procedure block", tname);
+           ffatalerr("lang.Proc.load(): Symbol '%s' not a procedure block", tname);
 
        free(tname);
        return proc(blk);
@@ -1329,7 +1331,7 @@ static struct b_proc *try_load(HMODULE handle, struct b_class *class0,  struct c
 
     /* Sanity check. */
     if (blk->title != T_Proc)
-        ffatalerr("lang.Class.load_library(): symbol %s not a procedure block", fq);
+        ffatalerr("lang.Class.load_library(): Symbol '%s' not a procedure block", fq);
 
     free(fq);
 
@@ -1370,10 +1372,12 @@ static HMODULE get_handle(char *filename)
     /* Check version number */
     version = (int *)GetProcAddress(handle, "oix_version");
     if (!version) {
+        FreeLibrary(handle);
         LitWhy("Symbol 'oix_version' not found");
         return 0;
     }
     if (*version != OixVersion) {
+        FreeLibrary(handle);
         whyf("Version mismatch (%d -vs- %d)", *version, OixVersion);
         return 0;
     }
@@ -1381,8 +1385,8 @@ static HMODULE get_handle(char *filename)
     /* Set imported variable */
     imported = (struct oisymbols **)GetProcAddress(handle, "imported");
     if (!imported) {
-        whyf("Symbol 'imported' not found in dll %s", filename);
         FreeLibrary(handle);
+        LitWhy("Symbol 'imported' not found");
         return 0;
     }
     *imported = &oiexported;
@@ -1468,7 +1472,7 @@ function lang_Proc_load(filename, funcname)
        }
        /* Sanity check. */
        if (blk->title != T_Proc)
-           ffatalerr("lang.Proc.load(): symbol %s not a procedure block", tname);
+           ffatalerr("lang.Proc.load(): Symbol '%s' not a procedure block", tname);
 
        free(tname);
        return proc(blk);
