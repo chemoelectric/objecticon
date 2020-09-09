5ede9991ead87d5750b056ddb3dbfdd03e6df73c
rparlett <rparlett@localhost>
2020-09-09 18:30:27 +0000

    Corrected clamping of scale_to destination

--- base/oix/fwindow.r
+++ base/oix/fwindow.r
@@ -2165,12 +2165,12 @@ function graphics_Pixels_scale_to(self, x0, y0, w0, h0, dest, a0, b0, c0, d0)
           for (i2 = 0; i2 < width2; ++i2) {
               fi = i2 * rw;
               i = (int)fi;
-              #define ClampW(i) Min(i, width - 1)
-              #define ClampH(i) Min(i, height - 1)
-              self_id->format->getpixel(self_id, ClampW(x + i     ), ClampH(y + j     ), &r00, &g00, &b00, &a00);
-              self_id->format->getpixel(self_id, ClampW(x + i + 1 ), ClampH(y + j     ), &r10, &g10, &b10, &a10);
-              self_id->format->getpixel(self_id, ClampW(x + i     ), ClampH(y + j + 1 ), &r01, &g01, &b01, &a01);
-              self_id->format->getpixel(self_id, ClampW(x + i + 1 ), ClampH(y + j + 1 ), &r11, &g11, &b11, &a11);
+              #define ClampW(v) Min(v, width - 1)
+              #define ClampH(v) Min(v, height - 1)
+              self_id->format->getpixel(self_id, x + ClampW( i     ), y + ClampH( j     ), &r00, &g00, &b00, &a00);
+              self_id->format->getpixel(self_id, x + ClampW( i + 1 ), y + ClampH( j     ), &r10, &g10, &b10, &a10);
+              self_id->format->getpixel(self_id, x + ClampW( i     ), y + ClampH( j + 1 ), &r01, &g01, &b01, &a01);
+              self_id->format->getpixel(self_id, x + ClampW( i + 1 ), y + ClampH( j + 1 ), &r11, &g11, &b11, &a11);
 
               r = bl_inter(r00, r10, r01, r11, fi - i, fj - j);
               g = bl_inter(g00, g10, g01, g11, fi - i, fj - j);
